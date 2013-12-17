11d0d8ffa14694c1146fbe5fcda72d9b80855d75
bock1221 <tzvibock@gmail.com>
2013-12-16 19:58:59 -0500

    unchanched book app files

---
+++ book_apps/ch01_test/src/TestApp.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zvibock
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x =14;
+        int y =8;
+        int result1 =x+y;
+        String a = "house"+y;
+        System.out.println(a);
+        a +=x;
+        System.out.println(a);
+        String b ="";
+        if(b.equals(""))
+            System.out.println("works");
+        // TODO code application logic here
+    }
+}
