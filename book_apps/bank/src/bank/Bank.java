51cda6aa59dfabffabd01d77f97ae88fd3d16bd0
bock1221 <tzvibock@gmail.com>
2013-12-29 12:58:01 -0500

    my own bank account project with inheritance

---
+++ book_apps/bank/src/bank/Bank.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author zvibock
+ */
+public class Bank {
+
+   
+    public static void main(String[] args) {
+        public class Account{
+    protected String firstName;
+    protected String lastName;
+    protected String addres;
+    protected String phoneNumber; 
+    protected ArrayList<Transactions> transactions= new ArrayList<Transactions>(); 
+    protected double currentBalance;
+    protected double totalMonthleyDeposits ;
+    protected double totalMonthleywithdrawels;
+    
+}
+    }
+}
---
+++ book_apps/lubowskiinheritence/src/lubowskiinheritence/Lubowskiinheritence.java
@@ -0,0 +1,13 @@
+
+package lubowskiinheritence;
+
+
+public class Lubowskiinheritence {
+
+
+   
+   
+    public static void main(String[] args) {
+       Account account=new account 
+    }
+}
